25cdcc1cf2d9457199aa15b5a9de04a22253ebfe
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-27 18:01:41 -0400

    cmd/shaw: use `shaw` package

--- cmd/shaw/shaw.go
+++ cmd/shaw/shaw.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
 
+	"deedles.dev/shaw"
 	"deedles.dev/shaw/dict"
-	"deedles.dev/xiter"
 )
 
 func loadDict(path string) (*dict.Dict, error) {
@@ -19,10 +18,6 @@ func loadDict(path string) (*dict.Dict, error) {
 	defer file.Close()
 
 	return dict.Parse(file)
-}
-
-func isTranslatable(r rune) bool {
-	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || r == '\''
 }
 
 func main() {
@@ -39,20 +34,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	s := bufio.NewScanner(os.Stdin)
-	for s.Scan() {
-		words := xiter.ChunksFunc(xiter.Runes(s.Bytes()), isTranslatable)
-		for word := range words {
-			s := string(word)
-			if isTranslatable(word[0]) {
-				s = dict.Translate(s)
-			}
-			fmt.Print(s)
+	for w, err := range shaw.Translate(os.Stdin, dict) {
+		if err != nil {
+			slog.Error("read input", "err", err)
+			os.Exit(1)
 		}
-		fmt.Println()
-	}
-	if err := s.Err(); err != nil {
-		slog.Error("read input", "err", err)
-		os.Exit(1)
+
+		fmt.Print(w)
 	}
 }
