a588c8db00ce2d77d2d5bfaa8b7dd9aea6ce0e18
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-28 11:21:11 -0400

    shaw: implement translation entirely as an `io.Reader`

--- shaw.go
+++ shaw.go
@@ -3,169 +3,101 @@ package shaw
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"io"
 	"iter"
-	"os"
-	"runtime"
-	"strings"
+	"unsafe"
 
 	"deedles.dev/shaw/dict"
-	"deedles.dev/xiter"
 )
-
-// isTranslatable returns true if r is something expected in a
-// translatable word.
-func isTranslatable(r rune) bool {
-	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || r == '\''
-}
-
-// runes returns an iterator over runes read from r.
-func runes(r io.Reader) iter.Seq2[rune, error] {
-	return func(yield func(rune, error) bool) {
-		br := bufio.NewReader(r)
-		for {
-			c, _, err := br.ReadRune()
-			if err != nil {
-				if !errors.Is(err, io.EOF) {
-					yield(0, err)
-				}
-				return
-			}
-
-			if !yield(c, nil) {
-				return
-			}
-		}
-	}
-}
-
-// words returns an iterator over word segments. Despite the name, it
-// returns whitespace and other non-word things as well as continuous
-// runs between each. In other words,
-//
-//	an  example
-//
-// will yield
-//
-//	[]rune("an")
-//	[]rune("  ")
-//	[]rune("example")
-func words(runes iter.Seq[rune]) iter.Seq[[]rune] {
-	return xiter.ChunksFunc(runes, isTranslatable)
-}
-
-// translateLines translates words read from r in a line-by-line
-// manner using a [bufio.Scanner]. It is primarily for translating
-// from stdin. Like [words], it also yields intervening non-word
-// segments.
-func translateLines(r io.Reader, dict *dict.Dict) iter.Seq2[string, error] {
-	return func(yield func(string, error) bool) {
-		s := bufio.NewScanner(r)
-		for s.Scan() {
-			for word := range words(xiter.Runes(s.Bytes())) {
-				s := string(word)
-				if isTranslatable(word[0]) {
-					s = dict.Translate(s)
-				}
-				if !yield(s, nil) {
-					return
-				}
-			}
-			if !yield("\n", nil) {
-				return
-			}
-		}
-		if err := s.Err(); err != nil {
-			yield("", err)
-		}
-	}
-}
-
-// translateRunes translates words read from r. Like [words], it also
-// yields intervening non-word segments.
-func translateRunes(r io.Reader, dict *dict.Dict) iter.Seq2[string, error] {
-	return func(yield func(string, error) bool) {
-		runes := func(ryield func(rune) bool) {
-			for r, err := range runes(r) {
-				if err != nil {
-					yield("", err)
-					return
-				}
-
-				if !ryield(r) {
-					return
-				}
-			}
-		}
-
-		for w := range words(runes) {
-			s := string(w)
-			if isTranslatable(w[0]) {
-				s = dict.Translate(s)
-			}
-			if !yield(s, nil) {
-				return
-			}
-		}
-	}
-}
-
-// Translated returns an iterator that translates words read from r
-// using dict. It also yields intervening non-word segments in between
-// the words translated such that joining all of the yielded strings
-// in order will result in a fully translated piece of text with
-// whitespace and punctuation preserved.
-//
-// If at any point a non-nil error is yielded, it will be the last
-// thing yielded.
-func Translated(r io.Reader, dict *dict.Dict) iter.Seq2[string, error] {
-	if r == os.Stdin {
-		return translateLines(r, dict)
-	}
-	return translateRunes(r, dict)
-}
-
-// TranslateString translates a string of text.
-func TranslateString(input string, dict *dict.Dict) string {
-	var buf strings.Builder
-	for word := range Translated(strings.NewReader(input), dict) {
-		buf.WriteString(word)
-	}
-	return buf.String()
-}
 
 // This might be one of the worst things that I've ever written.
 type translator struct {
-	next func() (string, error, bool)
+	r    *bufio.Reader
+	dict *dict.Dict
 	buf  bytes.Buffer
+	err  error
 }
 
 // Translate returns an io.Reader that translates data coming from r
 // as it itself is read from.
 func Translate(r io.Reader, dict *dict.Dict) io.Reader {
-	// TODO: Reverse this. This function should be the main one and the
-	// iterator should be a convenience wrapper.
-	next, stop := iter.Pull2(Translated(r, dict))
-
-	t := translator{next: next}
-	runtime.SetFinalizer(&t, func(t *translator) { stop() })
-
-	return &t
+	return &translator{
+		r:    bufio.NewReader(r),
+		dict: dict,
+	}
 }
 
-func (t *translator) Read(buf []byte) (int, error) {
-	for len(buf) > t.buf.Len() {
-		text, err, ok := t.next()
+func (t *translator) Read(buf []byte) (n int, err error) {
+	for {
+		bn, _ := t.buf.Read(buf[n:])
+		n += bn
+		if n == len(buf) {
+			return n, nil
+		}
+		if t.err != nil {
+			return n, t.err
+		}
+
+		t.advance()
+	}
+}
+
+func (t *translator) advance() {
+	t.buf.Reset()
+
+	next, stop := iter.Pull(bytesseq(t.r, &t.err))
+	defer stop()
+
+	c, ok := next()
+	if !ok {
+		return
+	}
+	t.buf.WriteByte(c)
+
+	translatable := isTranslatable(c)
+	defer t.translate(translatable)
+
+	for {
+		c, ok := next()
 		if !ok {
-			return t.buf.Read(buf)
+			return
 		}
-		if err != nil {
-			n, _ := t.buf.Read(buf)
-			return n, err
+		if isTranslatable(c) != translatable {
+			return
 		}
-		t.buf.WriteString(text)
+		t.buf.WriteByte(c)
+	}
+}
+
+func (t *translator) translate(translatable bool) {
+	if !translatable {
+		return
 	}
 
-	return t.buf.Read(buf)
+	word := unsafe.String(unsafe.SliceData(t.buf.Bytes()), t.buf.Len())
+	word = t.dict.Translate(word)
+	t.buf.Reset()
+	t.buf.WriteString(word)
+}
+
+// isTranslatable returns true if r is something expected in a
+// translatable word.
+func isTranslatable(r byte) bool {
+	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || r == '\''
+}
+
+func bytesseq(r io.ByteScanner, perr *error) iter.Seq[byte] {
+	return func(yield func(byte) bool) {
+		for {
+			c, err := r.ReadByte()
+			if err != nil {
+				*perr = err
+				return
+			}
+			if !yield(c) {
+				r.UnreadByte()
+				return
+			}
+		}
+	}
 }
